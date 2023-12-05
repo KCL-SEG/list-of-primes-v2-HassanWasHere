f4857270645e50796e1ee0c13f82b746645f1c51
Hassan Mahmood <hassanmahmood@live.co.uk>
2023-12-05 22:43:08 +0000

    Add prime number method

--- primes.py
+++ primes.py
@@ -3,4 +3,15 @@
 
 def primes(number_of_primes):
     list = []
+    i = 1
+    while len(list) < number_of_primes:
+        prime = True
+        for j in range(2, 1+(i//2)):
+            if i % j == 0:
+                prime = False
+                break
+        if prime:
+            list.append(i)
+        i+= 1
     return list
+
